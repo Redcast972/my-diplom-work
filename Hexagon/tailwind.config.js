3ae0801640c4f5867b6765e57c073ee0983f9656
beken_magzhan <m_beken@gosreestr.kz>
2025-06-10 21:43:26 +0500

    new styles

--- Hexagon/tailwind.config.js
+++ Hexagon/tailwind.config.js
@@ -1,34 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './Views/**/*.cshtml', // Razor
-    './wwwroot/**/*.html', // HTML
-    './Scripts/**/*.js',   // JavaScript
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
-      },
-      colors: {
-        accent: {
-          pink: '#ff2d8b',
-          violet: '#a043ff',
-          cyan: '#17d4fc',
+    darkMode: 'class',
+    content: [
+        './Views/**/*.cshtml', // Razor
+        './wwwroot/**/*.html', // HTML
+        './Scripts/**/*.js',   // JavaScript
+    ],
+    theme: {
+        extend: {
+            opacity: ['hover'],
+            fontFamily: {
+                sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+            },
+            colors: {
+                accent: {
+                    pink: '#ff2d8b',
+                    darkpink: '#e60067',
+                    violet: '#a043ff',
+                    cyan: '#17d4fc',
+                },
+            },
+            keyframes: {
+                float: {
+                    '0%, 100%': { transform: 'translateY(0)' },
+                    '50%': { transform: 'translateY(-10px)' },
+                },
+            },
+            animation: {
+                float: 'float 6s ease-in-out infinite',
+            },
         },
-      },
-      keyframes: {
-        float: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-10px)' },
-        },
-      },
-      animation: {
-        float: 'float 6s ease-in-out infinite',
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
